030d6e40b00084ff1ca888ed02378e603eef0606
shahriar <shahriareshraghi22@gmail.com>
2022-06-23 19:35:15 +0430

    fix inputs v-model name

--- src/components/NumericSeparatedInput.vue
+++ src/components/NumericSeparatedInput.vue
@@ -35,8 +35,9 @@ export default {
       if (e.keyCode === 8 && e.target.value === '') this.$refs.digitInput[Math.max(0, index - 1)].focus();
     },
     onInput(index) {
-      const [first, ...rest] = this.verifyCode[index].replace(/[^0-9]/g, '');
-      this.verifyCode[index] = first ?? ''; // the `??` '' is for the backspace usecase
+      const [first, ...rest] = this.inputValue[index].replace(/[^0-9]/g, '');
+      // this.inputValue[index] = (first === null || first === undefined) ? '' : first;
+      // this.inputValue[index] = first ?? ''; // the `??` '' is for the backspace usecase
       const lastInputBox = index === this.$refs.digitInput.length - 1;
       const insertedContent = first !== undefined;
       if (insertedContent && !lastInputBox) {
@@ -45,7 +46,7 @@ export default {
         this.$refs.digitInput[index + 1].value = rest.join('');
         this.$refs.digitInput[index + 1].dispatchEvent(new Event('input'));
       }
-      const code = this.verifyCode.map((value) => value).join('');
+      const code = this.inputValue.map((value) => value).join('');
       this.isInputFocused = code.length !== 4;
       this.$emit('code', code);
     },
